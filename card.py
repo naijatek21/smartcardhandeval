5f2a536b54bd5a1de40127eb52eae34a713d7699
Chikezie Egbuchulam <egbuchulamchike@gmail.com>
2024-01-04 17:13:05 -0500

    value dictionary added

--- card.py
+++ card.py
@@ -10,12 +10,25 @@ class Card:
         self.suit = suit
         self.value = value
 
-    face_dict={"A":14, "K":13 , "Q":12, "J":11}
+    valdict={"A":14, 
+              "K":13 , 
+              "Q":12, 
+              "J":11, 
+              "10":10, 
+              "9":9, 
+              "8":8,
+              "7":7 , 
+              "6":6,
+              "5":5,
+              "4":4,
+              "3":3,
+              "2":2,
+              }
     # when passed through as a string, with suit followed by a space then the rank 
     def __init__(self,str):
         x = str.split()
         self.suit = x[0]
-        self.value = x[1]
+        self.value = valdict[x[1]]
 
     def getVal(self):
         return self.value
